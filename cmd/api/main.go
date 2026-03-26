379cc9cf3bd69d1d6fd21a17a7a0f16bcd673e07
redjax <5534031+redjax@users.noreply.github.com>
2026-03-25 23:52:25 -0400

    db: Add db init to api startup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/redjax/serverbeacon/api"
+	"github.com/redjax/serverbeacon/internal/auth"
 	"github.com/redjax/serverbeacon/internal/config"
+	"github.com/redjax/serverbeacon/internal/db"
 	"github.com/spf13/pflag"
 )
 
@@ -24,8 +27,41 @@ func main() {
 
 	cfg := config.GetConfig()
 
+	// Database initialization
+	gdb, err := db.Open(cfg.DB.Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := db.Close(gdb); err != nil {
+			log.Printf("db close error: %v", err)
+		}
+	}()
+
+	// Do migrations
+	if err := auth.Migrate(gdb); err != nil {
+		log.Fatal(err)
+	}
+
+	// Create auth store for users & tokens
+	store := auth.NewStore(gdb)
+
+	// Bootstrap initial users & secrets
+	bootstrapSecrets, err := auth.EnsureBootstrap(store)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print secrets to console only (loggers will skip)
+	for k, v := range bootstrapSecrets {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", k, v)
+	}
+
+	// Initialize server
 	server := api.NewHttpServer(fmt.Sprintf("%d", cfg.APiSettings.Port))
 
+	// Start server
 	if err := server.ListenMulti("0.0.0.0"); err != nil {
 		log.Fatal(err)
 	}
